6759113ce9b4fdd6f7a995180c99fa7340c94316
Kjangir1910 <129332297+Kjangir1910@users.noreply.github.com>
2025-04-23 02:09:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './Home';
+import Services from './Services';
+import Blogs from './Blogs';
+import ContactUs from './ContactUs';
+import Navbar from './Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <> 
+    <Router>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/services' element={<Services/>}/>
+        <Route path='/blogs' element={<Blogs/>}/>
+        <Route path='/contactus' element={<ContactUs/>}/>
+      </Routes>
+    </Router>
+     </>
   );
 }
 
