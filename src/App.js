aaa067ff5657cfd2898e724a0f45ed05b3d37317
Kjangir1910 <129332297+Kjangir1910@users.noreply.github.com>
2025-04-23 02:18:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Home from './Home';
 import Services from './Services';
 import Blogs from './Blogs';
 import ContactUs from './ContactUs';
-import Navbar from './Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
