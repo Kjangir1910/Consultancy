5e501a07caa6aa360fa48c1192009426f7126827
Kjangir1910 <129332297+Kjangir1910@users.noreply.github.com>
2025-05-11 18:16:34 +0530

    Update BlogLayout.js

--- src/BlogLayout.js
+++ src/BlogLayout.js
@@ -3,7 +3,7 @@ import React from 'react';
 function BlogLayout({ children }) {
   return (
     <div className="container mt-5">
-      <h1 className="mb-4">Our Blog</h1>
+      <h1 className="mb-4">Our Blogs</h1>
       {children}
     </div>
   );
